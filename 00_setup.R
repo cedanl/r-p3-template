2ffbd761ee102467fe631a1c888bff110543919d
corneeldh <corneel@cefero.nl>
2025-02-12 13:28:17 +0100

    add: special setup functions script to distinguish betwen dev and setup, update 00_setup as well

--- 00_setup.R
+++ 00_setup.R
@@ -3,8 +3,12 @@
     Sys.setenv(R_CHECK_SYSTEM_CLOCK = 0)
 }
 
-source("utils/dev_functions.R")
+# Check and init project files, remove functions from environment afterwards
+source("utils/setup_functions.R")
 init_project_files()
+rm(generate_project_id,init_project_files, make_valid_package_name)
+
+source("utils/dev_functions.R")
 source("utils/manage_packages.R")
 
 load_all()
